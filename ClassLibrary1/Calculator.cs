ec5e1467e179da6a0abf0afe3c6a7bcd3ba8b7b4
atiraj <katiraj@gmail.com>
2016-06-19 10:08:46 +0530

    1st time

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class Calculator
+    {
+        public static int Calculate(int x, int y,char opr)
+        {
+            int result=0;
+            switch(opr)
+            {
+                case 'A':
+                    result = x + y;
+                    break;
+                case 'M':
+                    result = x - y;
+                    break;
+                default :
+                    result = 0;
+                    break;
+            }
+            return result;
+
+        }
+    }
+}
---
+++ NugetTestMS/CalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace NugetTestMS
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            bool ret;
+            int result=Calculator.Calculate(1,2,'A');
+            Assert.AreEqual(result,3);
+            
+        }
+    }
+}
